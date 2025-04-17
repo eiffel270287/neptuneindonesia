1f42ef223877c7e72486b4b2389b2b37b84d8d36
eiffel270287 <suryadisun87@gmail.com>
2025-04-17 15:29:26 +0900

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import NeptuneMain from './NeptuneMain.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <NeptuneMain />
-  </React.StrictMode>
-);
